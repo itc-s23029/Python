d5da8d0706f3f62a92773418e7e589c8f1b679fd
yamaki koudai <s23029@std.it-college.ac.jp>
2023-05-24 06:19:55 +0000

    add rensyu

---
+++ chao3/htvy6g3.py
@@ -0,0 +1,5 @@
+home = "amerika"
+if home == "amerika":
+    print("Hello, amerika!")
+else:
+    print("Hello, world!")
---
+++ chao3/p39.py
@@ -0,0 +1,3 @@
+home = "america"
+result = "t1" if home == "america" else "f1"
+print(result)
